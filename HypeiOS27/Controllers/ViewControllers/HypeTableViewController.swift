fdb37d43e4eeae08456b6fe0f9e218d9583dfce4
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2019-07-09 10:28:49 -0600

    storyboard setup

---
+++ HypeiOS27/Controllers/ViewControllers/HypeTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  HypeTableViewController.swift
+//  HypeiOS27
+//
+//  Created by Timothy Rosenvall on 7/9/19.
+//  Copyright © 2019 Timothy Rosenvall. All rights reserved.
+//
+
+import UIKit
+
+class HypeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "hypeCell", for: indexPath)
+        return cell
+    }
+
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+}
